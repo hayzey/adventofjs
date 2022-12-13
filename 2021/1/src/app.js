3cbffcf6fc235fb0f1d72e2d2ae3df7e190ac077
hayzey <12708007+hayzey@users.noreply.github.com>
2022-12-13 16:45:39 +1100

    Challenge 1: Timer loop working

---
+++ 2021/1/src/app.js
@@ -0,0 +1,60 @@
+(function() {
+    const minutesInput = document.getElementById("minutes");
+    const secondsInput = document.getElementById("seconds");
+    const startButton = document.getElementById("start");
+    const settingsButton = document.getElementById("settings");
+
+    let minutesValue = 15;
+    let secondsValue = 0;
+    let timerIntervalId = null;
+
+    const resumeTimer = () => {
+        const timerStartDate = new Date();
+
+        const msToAdd = (1000 * 60 * minutesValue) + (1000 * secondsValue);
+        const timerEndDate = new Date(timerStartDate.getTime() + msToAdd);
+        
+        timerIntervalId = setInterval(() => {
+            const now = new Date();
+            // const msElapsed = now.getTime() - timerStartDate.getTime();
+            const msRemaining = timerEndDate.getTime() - now.getTime();
+
+            const minutesRemaining = Math.floor(msRemaining / 1000 / 60);
+            const secondsRemaining = Math.round(msRemaining / 1000 % 60);
+
+            setMinutesValue(minutesRemaining);
+            setSecondsValue(secondsRemaining);
+        }, 1000);
+    };
+
+    const pauseTimer = () => {
+        if (timerIntervalId) {
+            clearInterval(timerIntervalId);
+            timerIntervalId = null;
+        }
+    };
+    
+    const formatField = (val) => {
+        let result = val.toString();
+
+        if (val < 10) {
+            result = `0${result}`;
+        }
+
+        return result;
+    };
+
+    const setMinutesValue = (val) => {
+        minutesValue = val;
+        minutesInput.value = formatField(val);
+    };
+
+    const setSecondsValue = (val) => {
+        secondsValue = val;
+        secondsInput.value = formatField(val);
+    };
+
+    startButton.addEventListener("click", () => {
+        resumeTimer();
+    });
+})();
